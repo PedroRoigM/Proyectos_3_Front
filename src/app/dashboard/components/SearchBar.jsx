62e1daed9511963ca8df6521c577ac93b4a286b0
Pedro Roig Morera <145972514+PedroRoigM@users.noreply.github.com>
2025-02-04 14:36:01 +0100

    Web structure

---
+++ src/app/dashboard/components/SearchBar.jsx
@@ -0,0 +1,4 @@
+// Componente: SearchBar
+// Devuelve una barra de búsqueda con un input y un botón de búsqueda que al hacer click en él, se ejecuta la función search que se le pasa por props.
+// Debe de dar sugerencias de búsqueda en tiempo real.
+// Debe admitir el botón intro para realizar la búsqueda.
