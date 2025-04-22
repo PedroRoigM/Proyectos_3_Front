748b831574701e4fc6dab745072594a919911d1d
Pedro Roig Morera <145972514+PedroRoigM@users.noreply.github.com>
2025-04-22 11:44:52 +0200

    Await a las cookies

--- src/app/dashboard/components/lib/AccountLogOutLogOut.js
+++ src/app/dashboard/components/lib/AccountLogOutLogOut.js
@@ -3,7 +3,7 @@ import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
 
 export default async function AccountLogOut() {
-    const cookieStore = cookies();
+    const cookieStore = await cookies();
     cookieStore.set('bytoken', '', { path: '/', expires: new Date(0) });
     redirect('/');
 }
